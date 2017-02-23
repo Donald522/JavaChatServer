5da1ed5d4fcf0589c29da0d3c04c6068adc540a6
Anton Tolkachev <anton.tolkachev@yandex.ru>
2017-02-23 13:37:56 +0300

    Fix test for socket provider

--- src/test/java/network/SocketProviderTest.java
+++ src/test/java/network/SocketProviderTest.java
@@ -1,14 +1,20 @@
 package network;
 
+import network.sender.Sender;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by Anton Tolkachev.
@@ -22,9 +28,17 @@ class SocketProviderTest {
     private Socket s1;
     private Socket s2;
 
+    Sender sender;
+
+    @BeforeAll
+    void mockSetUp() throws IOException {
+        sender = mock(Sender.class);
+        doNothing().when(sender).send(any(), any());
+    }
+
     @BeforeEach
     void setUp() {
-        socketProvider = new SocketProvider();
+        socketProvider = new SocketProvider(sender);
         executorService = Executors.newCachedThreadPool();
         s1 = new Socket();
         s2 = new Socket();
