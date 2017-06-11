75ce5576e79ff72a6afea1e79d13e3533fd55cb4
Anton Tolkachev <anton.tolkachev@yandex.ru>
2017-06-11 20:50:47 +0300

    Added mapping for new commands

--- src/main/java/network/Server.java
+++ src/main/java/network/Server.java
@@ -1,13 +1,28 @@
 package network;
 
-import dao.core.ClientSessionDao;
+import com.google.common.collect.ImmutableMap;
+import dao.core.impl.ClientSessionDao;
+import dao.core.impl.ContactListDao;
 import dao.util.DataSourceProvider;
 import dao.util.MysqlDataSourceProvider;
 import handler.ClientMessageParser;
 import handler.impl.ClientMessageParserImpl;
 import model.command.Command;
 import model.command.factory.CommandFactory;
-import model.command.impl.*;
+import model.command.impl.ApproveRequestCommand;
+import model.command.impl.ContactRequestCommand;
+import model.command.impl.CreateDialogCommand;
+import model.command.impl.DefaultCommand;
+import model.command.impl.GetContactsCommand;
+import model.command.impl.GetInboundRequestsCommand;
+import model.command.impl.GetOutboundRequestsCommand;
+import model.command.impl.RejectRequestCommand;
+import model.command.impl.RemoveContactCommand;
+import model.command.impl.SendMessageCommand;
+import model.command.impl.SignInCommand;
+import model.command.impl.SignOutCommand;
+import model.command.impl.SignUpCommand;
+import model.command.impl.UpdateProfileCommand;
 import network.receiver.Receiver;
 import network.receiver.impl.SimpleReceiver;
 import network.sender.Sender;
@@ -15,21 +30,26 @@ import network.sender.impl.SimpleSender;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import service.ClientSessionService;
+import service.ContactRequestSender;
+import service.ContactService;
 import service.DialogService;
 import service.MonitoringService;
 import service.StreamProvider;
 import service.impl.ClientSessionServiceImpl;
+import service.impl.ContactRequestSenderImpl;
+import service.impl.ContactServiceImpl;
 import service.impl.DialogServiceImpl;
 import service.impl.MonitoringServiceImpl;
 import service.impl.SocketStreamProvider;
 import storage.ClientSessionStorage;
+import storage.ContactListStorage;
 import util.Factory;
 
 import javax.security.auth.RefreshFailedException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -52,6 +72,10 @@ public class Server {
     private ClientSessionDao dao;
     private ClientSessionStorage storage;
     private DialogService dialogService;
+    private ContactListDao contactListDao;
+    private ContactListStorage contactListStorage;
+    private ContactRequestSender contactRequestSender;
+    private ContactService contactService;
     private SocketProvider socketProvider;
     private final StreamProvider streamProvider;
     private Sender sender;
@@ -76,20 +100,36 @@ public class Server {
         sender = new SimpleSender(parser, streamProvider);
         socketProvider = new SocketProvider(sender);
         dialogService = new DialogServiceImpl(sender);
-        service = new ClientSessionServiceImpl(dao, storage, dialogService, socketProvider);
-        factory = new CommandFactory(new HashMap<String, Command>(){{
-            put("signup", new SignUpCommand().withService(service));
-            put("signin", new SignInCommand().withService(service));
-            put("signout", new SignOutCommand().withService(service));
-            put("newdlg", new CreateDialogCommand().withService(service));
-            put("sendmsg", new SendMessageCommand().withService(service));
-            put("updpfl", new UpdateProfileCommand().withService(service));
-        }}).withDefaultValue(new DefaultCommand().withService(service));
+        contactListDao = new ContactListDao(dataSourceProvider.getDataSource());
+        contactListStorage = new ContactListStorage(contactListDao);
+        contactRequestSender = new ContactRequestSenderImpl(sender);
+        contactService = new ContactServiceImpl(storage, contactListStorage, contactRequestSender);
+        service = new ClientSessionServiceImpl(storage, dialogService, contactService, socketProvider);
+        factory = initializeFactory();
 
         receiver = new SimpleReceiver(streamProvider, parser, factory);
 
         monitoringService = new MonitoringServiceImpl(storage);
 
+    }
+
+    private Factory<?> initializeFactory() {
+        Map<String, Command> params = ImmutableMap.<String, Command>builder()
+                .put("signup", new SignUpCommand().withService(service))
+                .put("signin", new SignInCommand().withService(service))
+                .put("signout", new SignOutCommand().withService(service))
+                .put("newdlg", new CreateDialogCommand().withService(service))
+                .put("sendmsg", new SendMessageCommand().withService(service))
+                .put("updpfl", new UpdateProfileCommand().withService(service))
+                .put("frdrq", new ContactRequestCommand().withService(service))
+                .put("aprvrq", new ApproveRequestCommand().withService(service))
+                .put("rjctrq", new RejectRequestCommand().withService(service))
+                .put("rmfrnd", new RemoveContactCommand().withService(service))
+                .put("getfrnds", new GetContactsCommand().withService(service))
+                .put("getinrqs", new GetInboundRequestsCommand().withService(service))
+                .put("getoutrqs", new GetOutboundRequestsCommand().withService(service))
+                .build();
+        return new CommandFactory(params).withDefaultValue(new DefaultCommand().withService(service));
     }
 
     public Server() throws IOException, RefreshFailedException {
